a994e909286c0e88dbf9224a74960de40ecf3664
Danielius Goriunovas <danielius@adface.lt>
2025-09-30 23:32:32 +0300

    preparing for launch

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import UnoCSS from '@unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://portfolio.dago.lt',
+  base: '/',
   integrations: [UnoCSS()],
 });
