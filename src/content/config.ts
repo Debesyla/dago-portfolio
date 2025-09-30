c8123013b7cfd7858ae134d2eb977af75ebb4264
Danielius Goriunovas <danielius@adface.lt>
2025-09-30 23:00:47 +0300

    tech -> stack, also image array for multiple images

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,11 +2,11 @@ import { defineCollection, z } from 'astro:content';
 
 const projects = defineCollection({
   type: 'content',
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     date: z.date(),
-    tech: z.array(z.string()),
-    screens: z.array(z.string()).optional(),
+    stack: z.array(z.string()),
+    screens: z.array(image()).optional(),
   }),
 });
 
--- src/content/projects/hello-world/hello-world.md
+++ src/content/projects/hello-world/hello-world.md
@@ -2,7 +2,7 @@
 title: "Hello World Project"
 date: 2024-03-15
 stack: ["Astro", "UnoCSS"]
-screens: ["placeholder.png"]
+screens: ["placeholder.png", "placeholder-2.png"]
 ---
 
 test content
