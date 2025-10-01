bcbf91356786ccb8926685d3a72e862f6676c691
Danielius Goriunovas <danielius@adface.lt>
2025-10-01 23:43:12 +0300

    new data for projects: amount of users

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ const projects = defineCollection({
   schema: ({ image }) => z.object({
     title: z.string(),
     date: z.date(),
+    users: z.string().optional(),
     stack: z.array(z.string()),
     screens: z.array(z.object({
       image: image(),
